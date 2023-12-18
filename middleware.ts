ff917e2657bca2e9206525c8bff71109e0f7b60f
roman krasilnikov <krasilnikovroman5@gmail.com>
2023-12-18 19:52:32 +0700

    md 4

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,4 @@
-import { withAuth, NextRequestWithAuth } from "next-auth/middleware"
-import { NextResponse } from "next/server"
 
-export default withAuth(
-    // `withAuth` augments your `Request` with the user's token.
-    function middleware(request: NextRequestWithAuth) {
-
-        if (request.nextUrl.pathname.startsWith("/Admin")
-            && request.nextauth.token?.user.roles?.includes("ADMIN")) {
-            return NextResponse.rewrite(
-                new URL("/Admin", request.url)
-            )
-        }
-    },
-    {
-        callbacks: {
-            authorized: ({ token }) => !!token
-        },
-    }
-)
 
 export const config = {matcher:[
     "/",

ad9f5b2cb33d702e590856aab8ca08ba91a0c4d8
roman krasilnikov <krasilnikovroman5@gmail.com>
2023-12-18 20:23:53 +0700

    md auth

--- app/Auth/page.tsx
+++ app/Auth/page.tsx
@@ -1,4 +1,4 @@
-
+'use client'
 
 import UIbutton from '@/components/UIbutton'
 import UIinput from '@/components/UIinput'
@@ -6,14 +6,14 @@ import { useState } from 'react';
 import { signIn } from 'next-auth/react'
 
 export default function Auth() {
-    // const [login, setLogin] = useState<string>('')
-    // const [password, setPassword] = useState<string>('')
+    const [login, setLogin] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
 
     async function onSubmitLogIn(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
         const result = await signIn('credentials', {
-            username: '1',
-            password: '1',
+            username: login,
+            password: password,
             redirect: true,
             callbackUrl:"/main"
         })
@@ -28,12 +28,13 @@ export default function Auth() {
                     <p className=''>Вход</p>
                 </div>
                 <UIinput placeholder="Логин" header='Логин'
-                    
-                     />
+                    value={login}
+                    onChange={(event) => setLogin(event.target.value)} />
 
                 <UIinput placeholder="Пароль" header='Пароль'
                     type='password'
-                    />
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)} />
 
                 <UIbutton className='mt-4'>Вход</UIbutton>
             </form>
